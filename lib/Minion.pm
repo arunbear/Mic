119af3ee50b6daaf247742d653c596a72608f5b8
Arun Prasaad <arunbear@cpan.org>
2014-09-05 14:09:44 +0100

    Update docs.

--- lib/Minion.pm
+++ lib/Minion.pm
@@ -537,7 +537,45 @@ Minion - build and organise minions declaratively.
     
 =head1 DESCRIPTION
 
-Minion is a class builder oriented towards interface driven programming.
+Minion is a class builder that simplifies programming in the Object Oriented style I<as it was originally envisioned>
+i.e. where objects are "like biological cells and/or individual computers on a network, only able to communicate with messages"
+and "OOP to me means only messaging, local retention and protection and hiding of state-process, and extreme late-binding of all things."
+(see L<The Deep Insights of Alan Kay|http://mythz.servicestack.net/blog/2013/02/27/the-deep-insights-of-alan-kay/> for further context).
+
+Classes are built from a specification that declares the interface of the class (i.e. what commands minions of the classs respond to),
+as well as other packages that provide the implementation of these commands.
+
+=head1 USAGE
+
+=head2 Minion->minionize([HASHREF])
+
+To build a classs, call the C<minionize()> class method, with an optional hashref that specifies the class.
+If the hashref is not given, the specification is read from a package variable named C<%__Meta> in the package
+from which C<minionize()> was called.
+
+The meaning of the keys in the specification hash are described next.
+
+=head3 interface => ARRAYREF
+
+A reference to an array containing the messages that minions belonging to this class should respond to.
+An exception is raised if this is empty or missing.
+
+The messages named in this array must have corresponding subroutine definitions in a declared implementation
+or role package, otherwise an exception is raised.
+
+=head3 implementation => STRING
+
+The name of a package that defines the subroutines declared in the interface.
+
+The package may also contain other subroutines not declared in the interface that are for internal use in the package.
+These won't be callable using the C<$minion-E<gt>command(...)> syntax.
+
+=head3 roles => ARRAYREF
+
+A reference to an array containing the names of one or more Role packages that define the subroutines declared in the interface.
+
+The packages may also contain other subroutines not declared in the interface that are for internal use in the package.
+These won't be callable using the C<$minion-E<gt>command(...)> syntax.
 
 =head1 AUTHOR
 
