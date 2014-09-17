8beb6fb128ae0a4630b512e29800ebd970f588d8
Arun Prasaad <arunbear@cpan.org>
2014-09-17 13:49:59 +0100

    Streamline the synopsis.

--- lib/Minion.pm
+++ lib/Minion.pm
@@ -484,70 +484,6 @@ Minion - Spartans! What is I<your> API?
     
     # Now some 'normal' examples
     
-    use Test::Most tests => 4;
-    use Minion;
-    
-    my %Class = (
-        interface => [qw( next )],
-    
-        implementation => {
-            methods => {
-                next => sub {
-                    my ($self) = @_;
-    
-                    $self->{$$}{count}++;
-                }
-            },
-            has  => {
-                count => { default => 0 },
-            }, 
-        },
-    );
-    
-    my $counter = Minion->minionize(\%Class)->new;
-    
-    is $counter->next => 0;
-    is $counter->next => 1;
-    is $counter->next => 2;
-    
-    throws_ok { $counter->new } qr/Can't locate object method "new"/;
-    
-    
-    # Like above but give the class a name
-  
-    use Test::Most tests => 4;
-    use Minion;
-    
-    my %Class = (
-        name => 'Counter',
-        interface => [qw( next )],
-    
-        implementation => {
-            methods => {
-                next => sub {
-                    my ($self) = @_;
-    
-                    $self->{$$}{count}++;
-                }
-            },
-            has  => {
-                count => { default => 0 },
-            }, 
-        },
-    );
-    
-    Minion->minionize(\%Class);
-    my $counter = Counter->new;
-    
-    is $counter->next => 0;
-    is $counter->next => 1;
-    
-    throws_ok { $counter->new } qr/Can't locate object method "new"/;
-    throws_ok { Counter->next } qr/Can't locate object method "next" via package "Counter"/;
-    
-    
-    # Or put code in packages
-    
     package Example::Synopsis::Counter;
     
     use strict;
@@ -555,6 +491,7 @@ Minion - Spartans! What is I<your> API?
     
     our %__Meta = (
         interface => [qw( next )],
+    
         implementation => 'Example::Synopsis::Acme::Counter',
     );
     Minion->minionize;  
