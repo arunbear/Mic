12b89188c303d3d1c3ef3b28bf48d2b3d887ac06
Arun Prasaad <arunbear@gmail.com>
2014-12-22 19:16:13 +0000

    Add attribute symbol key to Implementations/Roles

--- lib/Minions/Implementation.pm
+++ lib/Minions/Implementation.pm
@@ -1,6 +1,7 @@
 package Minions::Implementation;
 
 use strict;
+use Minions::_Guts;
 use Package::Stash;
 
 sub import {
@@ -17,8 +18,14 @@ sub define {
     
     my $caller_pkg = delete $arg{-caller} || (caller)[0];
     my $stash = Package::Stash->new($caller_pkg);
+    
     $class->update_args(\%arg);
+    
     $stash->add_symbol('%__Meta', \%arg);
+    $stash->add_symbol(
+        sprintf('$%s', $arg{attribute_sym} || 'A'),
+        $Minions::_Guts::attribute_sym
+    );
 }
 
 sub update_args {}
---
+++ lib/Minions/_Guts.pm
@@ -0,0 +1,7 @@
+package Minions::_Guts;
+
+use Digest::MD5 qw( md5_hex );
+
+*attribute_sym = \ (substr(md5_hex($$), 0 ,8) . '.');
+
+1;
