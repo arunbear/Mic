0c0376510e60d400d8f3a24d4785d5d30f385de1
Arun Prasaad <arunbear@gmail.com>
2014-12-25 23:28:40 +0000

    Allow fall back to no attribute vars

--- lib/Minions/_Guts.pm
+++ lib/Minions/_Guts.pm
@@ -4,4 +4,17 @@ use Digest::MD5 qw( md5_hex );
 
 *attribute_sym = \ substr(md5_hex($$), 0 ,8);
 
+our %obfu_name;
+
+sub obfu_name {
+    my ($name, $spec) = @_;
+
+    if ($spec->{no_attribute_vars} || ! $obfu_name{$name}) {
+        return "-$name";        
+    }
+    else {
+        return $obfu_name{$name};
+    }
+}
+
 1;
