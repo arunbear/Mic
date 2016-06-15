33428c2e654b2b0fff8faafa3894f1f6a2dcf32c
Arun Prasaad <arunbear@cpan.org>
2016-06-15 11:03:36 +0100

    Update example per new constructor syntax

--- lib/Moduloop/Implementation.pm
+++ lib/Moduloop/Implementation.pm
@@ -463,11 +463,13 @@ Here is the interface for this fixed size queue
     use Moduloop
         interface => [qw( push pop size )],
 
-        construct_with  => {
-            max_size => { 
-                assert => { positive_int => sub { $_[0] =~ /^\d+$/ && $_[0] > 0 } }, 
-            },
-        }, 
+        constructor => {
+            kv_args => {
+                max_size => { 
+                    callbacks => { positive_int => sub { $_[0] =~ /^\d+$/ && $_[0] > 0 } }, 
+                },
+            }
+        },
 
         implementation => 'Example::Delegates::Acme::FixedSizeQueue_v1',
     ;
