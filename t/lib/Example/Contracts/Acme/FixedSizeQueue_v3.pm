1ac8321a54c94877b54fc924a6e04fd56ca966c5
Arun Prasaad <arunbear@cpan.org>
2016-06-16 17:44:07 +0100

    More postcondition tests

---
+++ t/lib/Example/Contracts/Acme/FixedSizeQueue_v3.pm
@@ -0,0 +1,41 @@
+package Example::Contracts::Acme::FixedSizeQueue_v3;
+
+use Example::Delegates::Queue;
+
+use Moduloop::Implementation
+    has  => {
+        q => { 
+            default => sub { Example::Delegates::Queue->new },
+        },
+
+        max_size => { 
+            init_arg => 'max_size',
+            reader   => 'max_size',
+        },
+    }, 
+    forwards => [
+        {
+            send => [qw( head tail pop )],
+            to   => 'q'
+        },
+    ],
+;
+
+sub size { 
+    my ($self) = @_;
+
+    # make postcondition fail
+    0;
+}
+
+sub push {
+    my ($self, $val) = @_;
+
+    $self->{$Q}->push($val);
+
+    if ($self->size > $self->{$MAX_SIZE}) {
+        $self->pop;        
+    }
+}
+
+1;
