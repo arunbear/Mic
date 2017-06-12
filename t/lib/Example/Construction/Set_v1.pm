0a1b93e635df5c2308f32222cae8773f4261096c
Arun Prasaad <arunbear@cpan.org>
2017-06-12 17:55:35 +0100

    Not needed as default constructor suffices

--- t/lib/Example/Construction/Set_v1.pm
+++ t/lib/Example/Construction/Set_v1.pm
@@ -3,13 +3,6 @@ package Example::Construction::Set_v1;
 use Moduloop
 
     interface => [qw( add has size )],
-    constructor => { kv_args => { items => {} } },
-
-    build_args => sub {
-        my ($class, @items) = @_;
-
-        return { items => \@items };
-    },
 
     implementation => 'Example::Construction::Acme::Set_v1',
 ;
